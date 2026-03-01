852bcc90381c2fb1e518810208d7251fee67e80d
JateenDhaduk <jateendhaduk456@gmail.com>
2026-03-01 10:53:59 +0000

    learn variable

---
+++ 01basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 1445
+let emailId = "jateendhaduk456@gmail.com"
+var accountPassword = "123445"
+accountCity = "jaipur"
+
+// accountId = 3 // not allowed;
+emailId = "jateendhaduk@gmail.com"
+accountPassword  = "1234454"
+accountCity = "surat"
+
+/*  
+pefer not to use var becasue of issue in block scope and function scope
+*/
+console.table([accountId , emailId , accountPassword , accountCity])
