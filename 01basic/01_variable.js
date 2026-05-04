87f72f36c7c77dc93bd47c903d17af21d3ec0698
JateenDhaduk <jateendhaduk456@gmail.com>
2026-05-04 14:31:59 +0530

    understand variable in js

---
+++ 01basic/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 1234567890;
+let email = "jateendhaduk@example.com";
+var accountPassword = "mySecurePassword123";
+accountCity = "surat";// not to be used without declaration, it will create a global variable which is not recommended
+// accountId = 12344; this is not allowed because accountId is a constant variable
+let accoutState;
+email ="jateendhaduk2@example.com";
+accountPassword = "myNewSecurePassword456";
+accountCity = "mumbai";
+console.table([accountId, email, accountPassword, accountCity, accoutState]);
+
+/*
+In JavaScript, there are three ways to declare variables: using `var`, `let`, and `const`.
+perfer not to use `var` because it has function scope and can lead to unexpected behavior.
+*/
