523e5aee48c01de69b8686f646ccb48540db20c7
JateenDhaduk <jateendhaduk456@gmail.com>
2026-05-04 15:02:45 +0530

    datatypes

---
+++ 01basic/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all code as newer version of JavaScript
+
+// alert(3+3); // we are using node js not browser, so alert will not work
+
+// code must be readable and future proof 
+
+let name = "Jateen Dhaduk";
+let age = 23;
+let isLoggIn = false;
+
+// number => 2 power of 53 -1 to 2 power of 53 +1
+// null => standlone value that represents "nothing" or "empty"
+// undefined => variable that has been declared but not assigned value
+// string => sequence of characters
+// boolean => true or false
+// symbol => unique identifier
+
+// object 
+
+console.log(typeof name);// tell us the type of variable
+console.log(typeof null)// object 
+console.log(typeof undefined) // undefined
