edce81239978cd884dce15eaad53bf30fdc941ad
JateenDhaduk <jateendhaduk456@gmail.com>
2026-05-05 14:42:19 +0530

    Conversion of datatype

---
+++ 01basic/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let score = true;
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber) // NaN = not a number 
+
+// "33" => 33
+// "33adv" => NaN
+// "jateen" => NaN
+// null => 0
+// true => 1 and false => 0
+
+let loggedIn = 5;
+
+let booleanloggedIn = Boolean(loggedIn);
+
+console.log(typeof booleanloggedIn);
+
+console.log(booleanloggedIn)
+
+// "" => false
+// "jateen" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber)
+
