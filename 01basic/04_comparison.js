907a7b1fd8bba830382a8c0385c13c3ebf882575
JateenDhaduk <jateendhaduk456@gmail.com>
2026-05-05 15:15:30 +0530

    add comparsion

---
+++ 01basic/04_comparison.js
@@ -0,0 +1,28 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log(2 < "1");
+
+console.log("2" > true);
+console.log(true == 1);
+console.log(true === 1);
+
+//=== strict check it also check datatype if datatype are diffrent it will give false
+
+// note that first always check is varible are same type before comparison 
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null <= 0);
+// aovid this type of comparison 
+// note 2 the reson that == and comaparison > < >= <= works diffrently 
+// comparison convert null to number 0 after that it compaare that why == give false and <= give true 
+
+
+
+
