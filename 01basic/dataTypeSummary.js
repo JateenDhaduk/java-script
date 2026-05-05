0533dd731f8e2ed4064bd10f1909f1877eac9c63
JateenDhaduk <jateendhaduk456@gmail.com>
2026-05-05 15:41:38 +0530

    summary datatype

---
+++ 01basic/dataTypeSummary.js
@@ -0,0 +1,41 @@
+// primitive 
+
+// 7 types : String , Number , Boolean , null , undefined , Symbol,
+// bigInt
+
+const score = 100 
+const scoreValue = 100.3
+
+const isLoggIn = false;
+const outsideTemp = null 
+
+const id = Symbol('123')
+const anotherId = Symbol('123') // this is not same as id 
+
+
+// Reference (Non primitive)
+
+// array , Object , function 
+
+const heros = ["ironman", "spiderman" , "hulk"]
+
+let myObj = {
+  name:"jateen",
+  age:22
+}
+
+const myFunction = function(){
+  console.log("hello world");
+}
+
+// function =>objectFunction 
+// array => object 
+// string => string 
+// symbol => symbol
+// object => object 
+//null => object
+// undefined => undefined
+// number => number
+// boolean => boolean
+
+
