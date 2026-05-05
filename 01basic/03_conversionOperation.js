f2c24997f59cbfe67281935af6992c55b08783fd
JateenDhaduk <jateendhaduk456@gmail.com>
2026-05-05 15:01:16 +0530

    add operation

--- 01basic/03_conversionOperation.js
+++ 01basic/03_conversionOperation.js
@@ -8,28 +8,74 @@
 
 // console.log(valueInNumber) // NaN = not a number 
 
-// "33" => 33
-// "33adv" => NaN
-// "jateen" => NaN
-// null => 0
-// true => 1 and false => 0
+// // "33" => 33
+// // "33adv" => NaN
+// // "jateen" => NaN
+// // null => 0
+// // true => 1 and false => 0
 
-let loggedIn = 5;
+// let loggedIn = 5;
 
-let booleanloggedIn = Boolean(loggedIn);
+// let booleanloggedIn = Boolean(loggedIn);
 
-console.log(typeof booleanloggedIn);
+// console.log(typeof booleanloggedIn);
 
-console.log(booleanloggedIn)
+// console.log(booleanloggedIn)
 
-// "" => false
-// "jateen" => true
+// // "" => false
+// // "jateen" => true
 
-let someNumber = 33;
+// let someNumber = 33;
 
-let stringNumber = String(someNumber)
+// let stringNumber = String(someNumber)
 
-console.log(stringNumber);
+// console.log(stringNumber);
 
-console.log(typeof stringNumber)
+// console.log(typeof stringNumber)
+
+
+// ********* Operations ******* // 
+
+let value =3 
+
+let negValue = -value
+
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+let firstName = "jateen"
+let lastName = " dhaduk"
+let fullName = firstName + lastName;
+console.log(fullName);
+
+console.log("1" + 2);
+console.log(1 + "2"); // string is first string conversion is perform 
+console.log("1" + 2 +2);
+console.log(1 + 2 + "2");
+// dont dependent on this type of conversion 
+// use peranthisis to make it clear
+console.log("1" + (2 + 2));
+console.log((1 + 2) + "2");
+
+console.log(+true) // give you 1 
+console.log(+""); // give you 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
 
