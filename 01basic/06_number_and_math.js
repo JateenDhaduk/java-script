1f0f25f4e569de6bb6b69febedffd3fab7ce4d3d
JateenDhaduk <jateendhaduk456@gmail.com>
2026-05-06 11:28:44 +0530

    add math and Number

---
+++ 01basic/06_number_and_math.js
@@ -0,0 +1,43 @@
+const score = 400 // js ditect and assign this is number 
+//console.log(score);
+
+const balance  = new Number(200) // explicity define the number 
+// console.log(balance);
+
+// console.log(typeof balance.toString());
+
+// console.log(balance.toFixed(2)); //Returns a string representing a number in fixed-point notation.
+
+const ontherNumber = 254.3433;
+
+//console.log(ontherNumber.toPrecision(1));
+
+const num = 1000000
+//console.log(num.toLocaleString('en-IN')); //Converts a number to a string by using the current or specified locale.
+
+// +++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.6));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor((Math.random() * 10 ) + 1));
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor((Math.random() * (max - min + 1)) + min));
+
+
+ 
+
+
+
+
+
+
+
+
