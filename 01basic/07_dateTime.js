d46a2eff6382dd095817944f6d9f5eef637ba555
JateenDhaduk <jateendhaduk456@gmail.com>
2026-05-06 12:04:12 +0530

    add date and time

---
+++ 01basic/07_dateTime.js
@@ -0,0 +1,23 @@
+let myDate = new Date() // date objet 
+console.log(myDate);
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toTimeString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toUTCString());
+
+
+let myNewDate = new Date(2004,0,12)
+let myNewDate1 = new Date(2003,10,28,10,6,17)
+console.log(myNewDate1.toDateString());
+
+
+
+
+
+
+
+
